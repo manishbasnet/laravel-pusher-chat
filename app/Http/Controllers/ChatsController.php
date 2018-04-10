bf4d62e8bd9d68c489288bc4e45c8b74c64acd82
manishbasnet <mbasnet911@gmail.com>
2018-04-10 15:39:16 +0545

    chat view working

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Message;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ChatsController extends Controller
